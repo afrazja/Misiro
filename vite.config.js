28b136646b0c1eb3c4c70cd2b16fbbf056f8892c
Afraz Javanmardi <45622413+afrazja@users.noreply.github.com>
2026-02-25 23:04:22 +0330

    feat: add vite-pwa plugin to generate service workers for TWA packaging

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			srcDir: './src',
+			mode: 'production',
+			strategies: 'generateSW',
+			registerType: 'autoUpdate',
+			manifest: false, // We already have a static manifest at static/site.webmanifest
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,json,mp3}']
+			}
+		})
+	]
 });
